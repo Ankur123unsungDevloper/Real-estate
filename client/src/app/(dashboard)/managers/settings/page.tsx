9d0f0e8f7903221cf8ead59431c34f701799d4ee
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-06-07 22:42:57 +0530

    modified:   client/src/app/(dashboard)/managers/settings/page.tsx

--- client/src/app/(dashboard)/managers/settings/page.tsx
+++ client/src/app/(dashboard)/managers/settings/page.tsx
@@ -13,15 +13,15 @@ const ManagerSettings = () => {
 
   if (isLoading) return <>Loading...</>;
 
-  const initialData = {
-    name: authUser?.userInfo.name,
-    email: authUser?.userInfo.email,
-    phoneNumber: authUser?.userInfo.phoneNumber,
-  };
+const initialData = {
+  name: authUser?.userInfo.name ?? "",
+  email: authUser?.userInfo.email ?? "",
+  phoneNumber: authUser?.userInfo.phoneNumber ?? "",
+};
 
   const handleSubmit = async (data: typeof initialData) => {
     await updateManager({
-      cognitoId: authUser?.cognitoInfo?.userId,
+      cognitoId: authUser?.cognitoInfo?.userId ?? "",
       ...data,
     });
   };
