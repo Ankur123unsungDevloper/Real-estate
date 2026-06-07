0dc112f432a7ab659555e8d9d64412bcbbf934c2
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-06-07 22:57:04 +0530

    modified:   client/src/hooks/use-mobile.ts 	modified:   client/src/state/redux.tsx

--- client/src/state/redux.tsx
+++ client/src/state/redux.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
@@ -30,15 +30,19 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 /* PROVIDER */
+
+// Create the store once at module level so it's never recreated
+const store = makeStore();
+
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore | null>(null);
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-    setupListeners(storeRef.current.dispatch);
-  }
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  useEffect(() => {
+    const unsubscribe = setupListeners(store.dispatch);
+    return unsubscribe;
+  }, []);
+
+  return <Provider store={store}>{children}</Provider>;
 }
